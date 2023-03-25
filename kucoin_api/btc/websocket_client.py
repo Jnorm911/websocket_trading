3445be0494d6a61c21afa339e04d5f9a3e395374
= t3nt4c135 <= j.norm911@gmail.com>
2023-03-25 13:11:17 -0700

    charting

--- kucoin_api/btc/websocket_client.py
+++ kucoin_api/btc/websocket_client.py
@@ -5,46 +5,20 @@ import pandas as pd
 from datetime import datetime
 from kucoin.client import WsToken
 from kucoin.ws_client import KucoinWsClient
+from websockets.exceptions import ConnectionClosedError
 from strategy.heikin_ashi import calculate_heikin_ashi
 
 symbol = "BTC-USDT"
 desired_timezone = pytz.timezone("America/Los_Angeles")
 
-dataframe = pd.DataFrame(
-    columns=[
-        "start_time",
-        "duration",
-        "ha_open",
-        "ha_close",
-        "ha_high",
-        "ha_low",
-        "volume",
-        "color",
-        "average_volume",
-    ]
-)
-
 start_time = int(time.time())
 
 
 previous_timestamp = 0
-dataframe = pd.DataFrame(
-    columns=[
-        "start_time",
-        "duration",
-        "ha_open",
-        "ha_close",
-        "ha_high",
-        "ha_low",
-        "volume",
-        "color",
-        "average_volume",
-    ]
-)
 
 
-async def handle_message(msg):
-    global dataframe, previous_timestamp
+async def handle_message(msg, dataframe):
+    global previous_timestamp
     if msg["type"] == "message":
         candles = msg["data"]["candles"]
         timestamp = int(candles[0])
@@ -57,13 +31,20 @@ async def handle_message(msg):
             utc_time = datetime.utcfromtimestamp(timestamp).replace(tzinfo=pytz.UTC)
             local_time = utc_time.astimezone(desired_timezone)
 
-            # Set the color to grey for the first row, otherwise use the calculated color
-            color = (
-                "grey" if dataframe.empty else "green" if ha_close > ha_open else "red"
-            )
+            # Set the color based on the current Heikin Ashi close price compared to the previous Heikin Ashi close price
+            if dataframe.empty:
+                color = "gray"
+            else:
+                prev_ha_close = dataframe.iloc[-1]["ha_close"]
+                if prev_ha_close < ha_close:
+                    color = "green"
+                elif prev_ha_close > ha_close:
+                    color = "red"
+                else:
+                    color = dataframe.iloc[-1]["color"]
 
             row = {
-                "start_time": local_time,  # Use local_time instead of timestamp
+                "start_time": local_time,
                 "duration": "1min",
                 "ha_open": ha_open,
                 "ha_close": ha_close,
@@ -81,28 +62,52 @@ async def handle_message(msg):
 
             previous_timestamp = timestamp
 
+    return dataframe
 
-async def print_dataframe():
-    global dataframe
-    while True:
-        await asyncio.sleep(60)
-        dataframe["average_volume"] = dataframe["volume"].sum() / len(dataframe)
-        print("Last 100 rows of the 1-minute dataframe:")
-        print(dataframe.tail(100))
+
+async def print_dataframe(dataframe):
+    dataframe["average_volume"] = dataframe["volume"].sum() / len(dataframe)
+    print(dataframe)  # Print DataFrame to console
+    dataframe.to_csv(
+        "C:\\Users\\jnorm\\Projects\\dataframe.csv", index=False
+    )  # Save DataFrame to CSV file
+    return dataframe
 
 
 async def main():
-    ws_token_client = WsToken()
-    ws_client = await KucoinWsClient.create(
-        None, ws_token_client, handle_message, private=False
+    global dataframe
+    dataframe = pd.DataFrame(
+        columns=[
+            "start_time",
+            "duration",
+            "ha_open",
+            "ha_close",
+            "ha_high",
+            "ha_low",
+            "volume",
+            "color",
+            "average_volume",
+        ]
     )
 
-    await ws_client.subscribe(f"/market/candles:{symbol}_1min")
-
-    asyncio.create_task(print_dataframe())  # Schedule the printing function
-
     while True:
-        await asyncio.sleep(15)
+        try:
+            ws_token_client = WsToken()
+            ws_client = await KucoinWsClient.create(
+                None, ws_token_client, None, private=False
+            )
+
+            await ws_client.subscribe(f"/market/candles:{symbol}_1min")
+
+            while True:
+                msg = await ws_client.recv()
+                dataframe = await handle_message(msg, dataframe)
+                dataframe = await print_dataframe(dataframe)
+                await asyncio.sleep(60)
+        except ConnectionClosedError as e:
+            print(f"Connection closed: {e}")
+            # Add custom reconnection logic or error handling here
+            await asyncio.sleep(5)  # Wait for 5 seconds before trying to reconnect
 
 
 if __name__ == "__main__":
